b275febf2f0b3a9af4f9df810bb4c55d7575c159
Ali hassan <aliihassan2003@gmail.com>
2025-11-10 11:27:29 +0100

    n

---
+++ script.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const mysql = require("mysql2");
+
+const cors = require("cors");
+
+const app = express();
+const port = 3005;
+
+app.use(cors());
+app.use(express.json());
+
+//Host, user, password database
+const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+});
+
+app.get('/', (req, res) => {
+    connection.query('SELECT `name` FROM Chinook', (error, results) => {
+        res.send(results);
+    });
+});
+pp.get('/', (req, res) => {
+    connection.query('SELECT `name` FROM Chinook', (error, results) => {
+        res.send(results);
+    });
+});
